f2130494a807f704bf8673b608b4498d1671ff3a
Bedis75 <bedisbrnsaid@gmail.com>
2026-07-28 16:03:44 +0100

    Update consumer with SQLite storage, ignore runtime db files

--- streaming/consumer.py
+++ streaming/consumer.py
@@ -2,6 +2,23 @@ import pandas as pd
 from kafka import KafkaConsumer
 import joblib
 import json
+import sqlite3
+from datetime import datetime
+
+
+
+conn = sqlite3.connect("predictions.db")
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS predictions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT,
+        prediction INTEGER,
+        real_value INTEGER,
+        label TEXT
+    )
+""")
+conn.commit()
 
 model = joblib.load("rf_model.pkl")
 
@@ -17,4 +34,10 @@ for message in consumer:
     df_row = pd.DataFrame([data])
     features = df_row.drop(columns=["Label","is_attack","source_day"])
     prediction = model.predict(features)
+    timestamp_value = datetime.now().isoformat()
     print("le modèle a prédit :", prediction[0], " | réel :", data["is_attack"], " | ", data["Label"])
+    cursor.execute(
+    "INSERT INTO predictions (timestamp, prediction, real_value, label) VALUES (?, ?, ?, ?)",
+    (timestamp_value, int(prediction[0]), int(data["is_attack"]), data["Label"])
+    )
+    conn.commit()
